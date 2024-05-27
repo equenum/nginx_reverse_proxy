4c79924ef2f562eda4f194547233da9cf4153ced
Egor Quenum <georgequenum@yahoo.com>
2024-05-27 21:32:29 +0400

    Add initial media service api

---
+++ MediaService.Api/Controllers/MediaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaService.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class MediaController : ControllerBase
+{
+    [HttpGet("stream")]
+    public IActionResult GetMediaStream()
+    {
+        Console.WriteLine("Call received!");
+
+        var bytes = new byte[100];
+        
+        // generate random bytes to imitate a workload
+        var random = new Random();
+        random.NextBytes(bytes);
+
+        return Ok(string.Join(" ", bytes));
+    }
+}
---
+++ MediaService.Api/Controllers/UserPreferencesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaService.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class UserPreferencesController : ControllerBase
+{
+    [HttpGet("lang")]
+    public async Task<IActionResult> GetLanguageAsync()
+    {
+        Console.WriteLine("Call received!");
+        
+        await Task.Delay(1000);
+        return Ok("English");
+    }
+}
